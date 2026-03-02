0c3dcc1537ab0fa2e69e446ff8d928fd107df3cf
RicardoFrancoDelgado <rf041555@gmail.com>
2026-03-02 11:12:51 -0300

    feat: Tipos de dados em Java

---
+++ src/TiposDeDados/DadosPrimitivos.java
@@ -0,0 +1,23 @@
+package TiposDeDados;
+
+public class DadosPrimitivos {
+    public static void main(String[] args) {
+        /*
+         * Dados primitivos - int, double, float, char, boolean, short.
+         * Objetivo - Criar um ninja - Naruto -
+         */
+
+        int idade = 16; // int valor máximo - 2147483647
+        double altura = 1.65;
+        char inicial = 'N';
+        boolean vivoOuMorto = true;
+        Long saldoBancario = 2147483648L; // Long valor máximo - 9,223,372,036,854,775,807
+
+        System.out.println(idade);
+        System.out.println(saldoBancario);
+        System.out.println(altura);
+
+        System.out.println("Saldo bancário é = " + saldoBancario);
+        System.out.println("Minha idade é = " + idade);
+    }
+}
