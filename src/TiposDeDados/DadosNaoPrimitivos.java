66a52e8ec64a4dff86d6cc1915fffa1fa159b033
RicardoFrancoDelgado <rf041555@gmail.com>
2026-03-02 11:47:30 -0300

    feat: Tipos de dados não primitivos

--- src/TiposDeDados/DadosNaoPrimitivos.java
+++ src/TiposDeDados/DadosNaoPrimitivos.java
@@ -2,6 +2,17 @@ package TiposDeDados;
 
 public class DadosNaoPrimitivos {
     public static void main(String[] args) {
+        /*
+        * Dados não primitivos: string, array, class, enum
+        * Objetivo - Criar um ninja e atribuir metodos a ele
+         */
 
+        String nome = "Naruto Uzumaki";
+        String nomeUpperCase = nome.toUpperCase();
+        System.out.println(nomeUpperCase);
+
+        String aldeia = "Aldeia da Folha";
+        String aldeiaLowerCase = aldeia.toLowerCase();
+        System.out.println(aldeiaLowerCase);
     }
 }
