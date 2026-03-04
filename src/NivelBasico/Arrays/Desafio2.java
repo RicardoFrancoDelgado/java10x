b2a02a1e2104f9296a63a1736839028d3af51550
RicardoFrancoDelgado <rf041555@gmail.com>
2026-03-04 12:16:48 -0300

    feat: desafio 2 - cadastro de ninjas

---
+++ src/NivelBasico/Arrays/Desafio2.java
@@ -0,0 +1,61 @@
+package NivelBasico.Arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Desafio2 {
+    public static void main(String[] args) {
+        // Cadastrar ninjas (com menu de opção)
+        Scanner scanner = new Scanner(System.in);
+
+        // ARRAY DE NINJAS
+        int NUMERO_MAX = 10;
+        String[] ninjas = new String[NUMERO_MAX];
+
+        // variaveis de controle
+        int ninjasCadastrados = 0;
+        int opcao = 0;
+
+        // DISPLAY PRO USUÁRIO
+        while (opcao != 3) {
+            // MENU
+            System.out.println("\n====== Cadastro de Ninjas ======");
+            System.out.println("1. Cadastrar um ninja");
+            System.out.println("2. Listar um ninja");
+            System.out.println("3. Sair");
+            System.out.print("Informa uma opção: ");
+            opcao = scanner.nextInt();
+            scanner.nextLine();
+
+            switch(opcao) {
+                case 1:
+                    if (ninjasCadastrados < NUMERO_MAX) {
+                        System.out.println("Informe o nome do ninja:");
+                        String nomeNinja = scanner.nextLine();
+                        ninjas[ninjasCadastrados] = nomeNinja;
+                        ninjasCadastrados++;
+                        System.out.println("Ninja cadastrado com sucesso!");
+                    } else {
+                        System.out.println("Limite de ninjas cadastrados");
+                    }
+                    break;
+                case 2:
+                    if (ninjasCadastrados == 0) {
+                        System.out.println("Nenhum ninja cadastrado");
+                    } else {
+                        System.out.println("\n========= Lista de Ninjas =========");
+                        for (int i = 0; i < ninjas.length; i++) {
+                            System.out.println(ninjas[i]);
+                        }
+                    }
+                    break;
+                case 3:
+                    System.out.println("Encerrando o programa...");
+                    break;
+                default:
+                    System.out.println("Opção inválida");
+                    break;
+            }
+        }
+    }
+}
