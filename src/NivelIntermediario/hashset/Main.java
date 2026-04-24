73faa547bd2ab1e396737f8df7a6587e78baaefe
RicardoFrancoDelgado <rf041555@gmail.com>
2026-04-23 21:28:40 -0300

    feat: HashSet, LinkedHashSet, TreeSet

--- src/NivelIntermediario/hashset/Main.java
+++ src/NivelIntermediario/hashset/Main.java
@@ -1,9 +1,6 @@
 package NivelIntermediario.hashset;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,15 +10,23 @@ public class Main {
         ninjas.add("Sasuke");
         ninjas.add("Sakura");
         ninjas.add("Naruto");
+        ninjas.add("Amaterasu");
+        ninjas.add("Boruto");
 
         System.out.println("Ninjas list = " + ninjas);
 
-        Set<String> ninjasSet = new HashSet<>(); // remove duplicatas
+        Set<String> ninjasHashSet = new HashSet<>(); // hashset remove duplicatas
+        Set<String> ninjasTreeSet = new TreeSet<>(); // tree remove duplicatas - colocando em ordem natural dos tipos
+        Set<String> ninjasLinkedHashSet = new LinkedHashSet<>(); // hashset remove duplicatas - exibe em ordem de declaração
 
-        ninjasSet.addAll(ninjas);
+        ninjasHashSet.addAll(ninjas);
+        ninjasTreeSet.addAll(ninjas);
+        ninjasLinkedHashSet.addAll(ninjas);
         // remove sem indice -> remove por objeto literal
 
-        System.out.println("Ninjas set = " + ninjasSet); // não exibe em ordem
+        System.out.println("Ninjas HashSet = " + ninjasHashSet); // não exibe em ordem
+        System.out.println("Ninjas TreeSet = " + ninjasTreeSet); // não exibe em ordem
+        System.out.println("Ninjas LinkedHashSet = " + ninjasLinkedHashSet); // não exibe em ordem
 
 
     }
